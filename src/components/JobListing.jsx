4e0c3a36a00227b63ef63cfa75d22aa49203317d
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2024-10-19 18:47:43 -0400

    change more/less --> Show More/Less

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -24,7 +24,7 @@ const JobListing = ({ job }) => {
                 <div className='mb-5'>{description}</div>
                 <button onClick={()=>setShowFullDescription((prevState)=>!prevState)}
                         className="text-indigo-500 mb-5 hover:text-indigo-700">
-                    {showFullDescription ? 'less' : 'more'}
+                    {showFullDescription ? 'Show Less' : 'Show More'}
                 </button>
                 <h3 className='text-indigo-580 mb-2'>
                     {job.salary} / Year
