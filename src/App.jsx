f9d208cb3e97c2ef5a6669155d60e5ffd5e7c81c
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2024-10-19 19:49:08 -0400

    change return from App, now use RouterProvider

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,15 @@ import Hero from "./components/Hero.jsx";
 import HomeCards from "./components/HomeCards.jsx";
 import JobListings from "./components/JobListings.jsx";
 import ViewAllJobs from "./components/ViewAllJobs.jsx";
+import {Route, createBrowserRouter,createRoutesFromElements,RouterProvider} from "react-router-dom";
+
+
+const router= createBrowserRouter(
+    createRoutesFromElements(<Route index element={<h1>MyApp</h1>}/>)
+);
+
 const App = () => {
-  return (
-    <>
-        <Navbar/>
-        <Hero/>
-        <HomeCards/>
-        <JobListings/>
-        <ViewAllJobs/>
-    </>
-  );
-}
+  return <RouterProvider router={router}/>;
+};
 
 export default App;
