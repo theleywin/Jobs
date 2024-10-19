2df0c79eab8eea48c512d42bdcd53aaa4b67d021
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2024-10-19 19:58:59 -0400

    added homepage

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,16 @@
+
+import Hero from "../components/Hero.jsx";
+import HomeCards from "../components/HomeCards.jsx";
+import JobListings from "../components/JobListings.jsx";
+import ViewAllJobs from "../components/ViewAllJobs.jsx";
+
+const HomePage = () => {
+
+    return (
+        <>
+            <Hero/>
+        </>
+    );
+}
+
+export default HomePage;
