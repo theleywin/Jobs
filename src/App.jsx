ee19e23905197e7702668c5a62963072eb243a0e
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2024-10-19 15:50:20 -0400

    v1.0

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import HomeCards from "./components/HomeCards.jsx";
+import JobListings from "./components/JobListings.jsx";
+import ViewAllJobs from "./components/ViewAllJobs.jsx";
+const App = () => {
+  return (
+    <>
+        <Navbar/>
+        <Hero/>
+        <HomeCards/>
+        <JobListings/>
+        <ViewAllJobs/>
+    </>
+  );
+}
+
+export default App;
