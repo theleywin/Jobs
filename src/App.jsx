85eef97a64fbeccbd2aca3323aef53acb3a2783a
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2024-10-19 20:07:10 -0400

    using MainLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout.jsx";
 import {Route, createBrowserRouter,createRoutesFromElements,RouterProvider} from "react-router-dom";
 
 
 const router= createBrowserRouter(
-    createRoutesFromElements(<Route index element={<HomePage/>}/>)
+    createRoutesFromElements(
+        <Route path="/" element={<MainLayout/>}>
+
+          <Route index element={<HomePage/>}/>
+
+        </Route>
+
+    )
 );
 
 const App = () => {
