cb39399ddd2b1340b2274fde0e12c14539491afb
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2024-10-19 21:42:19 -0400

    implement page

--- src/Pages/JobsPage.jsx
+++ src/Pages/JobsPage.jsx
@@ -1,10 +1,10 @@
-
+import JobListings from "../components/JobListings.jsx";
 
 const JobsPage = () => {
   return (
-    <div>
-      <h1>Jobs Page</h1>
-    </div>
+    <section className="bg-blue-50 px-4 py6-">
+        <JobListings/>
+    </section>
   );
 }
 
