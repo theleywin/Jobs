21ac72f721e2b2f126bf134b0c75a4560efa1f7c
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2024-10-19 21:13:14 -0400

    persistent color black on active link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,41 @@
 import logo from "../assets/images/logo.png";
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="bg-indigo-700 border-b border-indigo-500">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="flex h-20 items-center justify-between">
                 <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-                    <Link className="flex flex-shrink-0 items-center mr-4" to="/index">
+                    <NavLink className="flex flex-shrink-0 items-center mr-4" to="/index">
                         <img className="h-10 w-auto" src={logo} alt="Jobs"/>
                         <span className="hidden md:block text-white text-2xl font-bold ml-2">
                             Jobs
                         </span>
-                    </Link>
+                    </NavLink>
                     <div className=" md:ml-auto">
                         <div className="flex space-x-2">
-                            <Link className="text-white hover:bg-gray-900
-                               hover:text-white rounded-md px-3 py-2"
+
+                            <NavLink className={({isActive})=>
+                                isActive?"bg-black text-white hover:bg-gray-900 " +
+                                    "hover:text-white rounded-md px-3 py-2":"text-white hover:bg-gray-900\n" +
+                                    "                               hover:text-white rounded-md px-3 py-2"}
                                to="/">
                                 Home
-                            </Link>
-                            <Link className="text-white hover:bg-gray-900
-                               hover:text-white rounded-md px-3 py-2" to="/jobs">
+                            </NavLink>
+
+                            <NavLink className={({isActive})=>
+                                isActive?"bg-black text-white hover:bg-gray-900 " +
+                                    "hover:text-white rounded-md px-3 py-2":"text-white hover:bg-gray-900\n" +
+                                    "                               hover:text-white rounded-md px-3 py-2"} to="/jobs">
                                 Jobs
-                            </Link>
-                            <Link className="text-white hover:bg-gray-900
-                               hover:text-white rounded-md px-3 py-2" to="/add-job">
+                            </NavLink>
+
+                            <NavLink className={({isActive})=>
+                                isActive?"bg-black text-white hover:bg-gray-900 " +
+                                    "hover:text-white rounded-md px-3 py-2":"text-white hover:bg-gray-900\n" +
+                                    "                               hover:text-white rounded-md px-3 py-2"} to="/add-job">
                                 Add Job
-                            </Link>
+                            </NavLink>
                         </div>
                     </div>
                 </div>
