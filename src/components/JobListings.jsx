4f2e5b63b75eab843ea712709dbb61dd1d90b221
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2024-10-19 21:43:54 -0400

    act different betwen pages

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,15 +1,16 @@
 import jobs from '../jobs.json';
 import JobListing from "./JobListing.jsx";
-const JobListings = () => {
-    const recentJobs= jobs.slice(0,3);
+import PropTypes from 'prop-types';
+const JobListings = ({isHome = false}) => {
+    const joblistings= isHome ? jobs.slice(0,3) : jobs;
     return (
         <section className='bg-blue-50 px-4 py-10'>
             <div className='container-xl lg:container mx-auto'>
                 <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
-                    Browse Jobs
+                    {isHome? "Recent Jobs" : "Browse Jobs"}
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {recentJobs.map((job)=>(
+                    {joblistings.map((job)=>(
                         <JobListing key={job.id} job={job}/>
                     ))}
                 </div>
@@ -17,4 +18,9 @@ const JobListings = () => {
         </section>
     );
 }
+JobListings.propTypes = {
+    isHome: PropTypes.bool,
+};
+
 export default JobListings;
+
