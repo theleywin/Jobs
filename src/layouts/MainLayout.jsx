3b071a3c16c3db2ee7f82ddd71b862fd778657fe
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2024-10-19 20:14:39 -0400

    finish Homepage

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import {Outlet} from "react-router-dom";
+import Navbar from "../components/Navbar.jsx";
 
 const MainLayout = () => {
     return (
         <>
-        <Outlet/>
+            <Navbar/>
+            <Outlet/>
         </>
     )
 };
